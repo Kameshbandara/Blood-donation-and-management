b76437980de1675d981af3eb797da1690413f43d
Kameshbandara <85399866+Kameshbandara@users.noreply.github.com>
2022-09-15 12:00:46 +0530

    Add files via upload

---
+++ BDMS/footer.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+  <style>
+  #footer {
+
+  position:absolute;
+    
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 75px;
+  background-color:#347fd0;
+  box-shadow: inset 0 -15px 30px rgba(10,4,60,0.4), 0 5px 10px rgba(10,20,100,0.5);
+  color:white;
+  
+  text-align: center;
+}
+  </style>
+</head>
+<body>
+  <div id="footer" >
+  <b><center><h5>Follow Us on our Social Media </h5>
+              <p = class="h7">
+                <img src="image/facebook.png" width="16px" height="16px" alt="FBLogo" class="Icon">&nbsp;Facebook &nbsp; &nbsp; &nbsp; &nbsp;
+
+ 
+                <img src="image/youtube.png" width="16px" height="16px" alt="FBLogo" class="Icon">&nbsp;YouTube &nbsp; &nbsp; &nbsp; &nbsp;
+                <img src="image/twitter.png" width="16px" height="16px" alt="FBLogo" class="Icon">&nbsp;Twitter &nbsp; &nbsp; &nbsp; &nbsp;
+                <img src="image/instagram.png" width="16px" height="16px" alt="FBLogo" class="Icon">&nbsp;Instagram &nbsp; &nbsp; &nbsp; &nbsp;
+
+              </p>
+  </center>
+  </div>
+
+
+</body>
+
+</html>
