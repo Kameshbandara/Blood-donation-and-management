057093247fa1ec9a7d7f97c9c651036d5c7ec279
Kameshbandara <85399866+Kameshbandara@users.noreply.github.com>
2022-09-16 19:38:00 +0530

    Add files via upload

---
+++ BDMS/index.php
@@ -0,0 +1,12 @@
+<?php require_once('inc/connection.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset = "UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+</body>
+</html>	
+<?php mysqli_close ($connection); ?>
